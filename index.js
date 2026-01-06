f577b82a32aa3e650f86e82efbbafd6a4ebd9949
Dheeraj Kumawat <2310030198@klh.edu.in>
2026-01-06 14:59:46 +0530

    hi

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ function longestCommonPalindromicSubsequence(a, b) {
   return ans;
 }
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
